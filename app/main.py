2e5465f674bd0af3f824cae815a790a6b1181e65
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-17 23:07:05 -0600

    Added logic to create schema

--- app/main.py
+++ app/main.py
@@ -1,5 +1,9 @@
 import uvicorn
 from app.interfaces.voter_controller import app
+from app.infrastructure.database import engine, Base
+
+# Create tables in the database
+Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8000)
