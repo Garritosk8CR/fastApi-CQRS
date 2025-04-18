f171b113a139a53f8330b20839a914669d51ab89
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-18 16:48:38 -0600

    register election router in main

--- app/main.py
+++ app/main.py
@@ -3,11 +3,13 @@ from app.infrastructure.database import engine, Base
 import sqlalchemy
 from fastapi import FastAPI
 from app.interfaces.voter_controller import router as voter_router
+from app.interfaces.election_controller import router as election_router
 
 app = FastAPI()
 
 # Include the voter router
 app.include_router(voter_router, prefix="/voter", tags=["Voters"])
+app.include_router(election_router, prefix="/elections", tags=["Elections"])
 
 # Create tables in the database
 Base.metadata.create_all(bind=engine)
