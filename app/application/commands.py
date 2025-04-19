6cb0de64e690c0661e636e709d6826ca82c403ff
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-19 02:18:41 -0600

    Added command check if voter exists, we are separating read db if exists within a write handler which violates CQRS principle

--- app/application/commands.py
+++ app/application/commands.py
@@ -12,3 +12,7 @@ class CastVoteCommand(BaseModel):
 class CreateElectionCommand(BaseModel):
     name: str
     candidates: List[str]
+
+class CheckVoterExistsQuery:
+    def __init__(self, voter_id: int):
+        self.voter_id = voter_id
