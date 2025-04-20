6a5fe334ada6a1f7cbd3990864e57cd25afab07d
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-20 15:25:08 -0600

    Added add_voter to voter repo

--- app/infrastructure/voter_repo.py
+++ app/infrastructure/voter_repo.py
@@ -19,3 +19,10 @@ class VoterRepository:
     
     def voter_exists(self, voter_id: int) -> bool:
         return self.db.query(Voter).filter(Voter.id == voter_id).first() is not None
+    
+    def add_voter(self, voter_id: int, name: str):
+        new_voter = Voter(id=voter_id, name=name, has_voted=False)
+        self.db.add(new_voter)
+        self.db.commit()
+        self.db.refresh(new_voter)
+        return new_voter
