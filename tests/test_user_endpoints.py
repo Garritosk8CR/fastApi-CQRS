92eed203c34c5c23646cd057a4c8bd8760f576c1
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-20 23:42:46 -0600

    Added test create successful sign up

--- tests/test_user_endpoints.py
+++ tests/test_user_endpoints.py
@@ -14,3 +14,16 @@ def test_db():
     yield
     # Clean up the database after tests
     Base.metadata.drop_all(bind=engine)
+
+
+def test_successful_sign_up(test_db):
+    response = client.post(
+        "/users/sign-up",
+        json={
+            "name": "Test User",
+            "email": "testuser@example.com",
+            "password": "securepassword"
+        }
+    )
+    assert response.status_code == 201
+    assert response.json() == {"message": "User Test User registered successfully as a voter!"}
