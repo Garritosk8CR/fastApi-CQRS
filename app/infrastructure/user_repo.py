834b2c726924c7d328e9ac6feadadaebcf739ed5
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-20 22:20:12 -0600

    Added repo for User

---
+++ app/infrastructure/user_repo.py
@@ -0,0 +1,18 @@
+from app.infrastructure.models import User
+from app.utils.password_utils import hash_password
+
+
+class UserRepository:
+    def __init__(self, db):
+        self.db = db
+
+    def create_user(self, name: str, email: str, password: str, role: str = "voter"):
+        hashed_password = hash_password(password)  # Hash the password here
+        new_user = User(name=name, email=email, password=hashed_password, role=role)
+        self.db.add(new_user)
+        self.db.commit()
+        self.db.refresh(new_user)
+        return new_user
+
+    def get_user_by_email(self, email: str):
+        return self.db.query(User).filter(User.email == email).first()
