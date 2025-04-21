395b594b76bc459b84095acbadf355f473cbf4ad
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-21 04:14:28 -0600

    Added get current helper in security

--- app/security.py
+++ app/security.py
@@ -1,9 +1,12 @@
 from datetime import datetime, timedelta
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from passlib.context import CryptContext
-from jose import jwt
+from jose import JWTError, jwt
 from app.config import SECRET_KEY, ALGORITHM
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
 def hash_password(password: str) -> str:
     return pwd_context.hash(password)
@@ -19,3 +22,13 @@ def create_access_token(data: dict, expires_delta=None):
         to_encode.update({"exp": datetime.now(datetime.timezone.utc) + timedelta(minutes=15)})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        email = payload.get("sub")
+        if email is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return email
+    except JWTError:
+        raise HTTPException(status_code=401, detail="Invalid token")
