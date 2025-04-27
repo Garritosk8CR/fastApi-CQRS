e8ce7cdeacaaf43961a0abf60c3436b477df0020
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-27 00:42:21 -0600

    Added endpoint /{election_id}/candidate-support

--- app/interfaces/election_controller.py
+++ app/interfaces/election_controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from app.application.query_bus import query_bus
-from app.application.queries import GetAllElectionsQuery, GetElectionDetailsQuery, GetElectionResultsQuery
+from app.application.queries import CandidateSupportQuery, GetAllElectionsQuery, GetElectionDetailsQuery, GetElectionResultsQuery
 from app.application.commands import CreateElectionCommand, EndElectionCommand
 from app.infrastructure.models import ElectionResponse
 from app.application.commands import CreateElectionCommand
@@ -64,4 +64,18 @@ def get_election_results(election_id: int):
         raise HTTPException(status_code=404, detail=str(e))
     except Exception as e:
         raise HTTPException(status_code=500, detail="An unexpected error occurred")
+    
+@router.get("/{election_id}/candidate-support")
+def candidate_support(
+    election_id: int
+):
+    query = CandidateSupportQuery(election_id=election_id)
+
+    try:
+        result = query_bus.handle(query)
+        return {"candidates": result}
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
 
