ddc5af210970158426fce26b1ff2d5b10d118441
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-26 19:40:22 -0600

    Added user by role handler in the query bus

--- app/application/handlers.py
+++ app/application/handlers.py
@@ -404,6 +404,7 @@ query_bus.register_handler(GetUserProfileQuery, GetUserProfileHandler())
 query_bus.register_handler(HasVotedQuery, HasVotedHandler())
 query_bus.register_handler(GetUserByIdQuery, GetUserByIdHandler())
 query_bus.register_handler(ListAdminsQuery, ListAdminsHandler())
+query_bus.register_handler(UsersByRoleQuery, UsersByRoleHandler())
 
 
 
