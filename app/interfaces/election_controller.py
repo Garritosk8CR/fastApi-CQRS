41c7dba35ab48d506e4bc169aa24d53263313ee3
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-28 23:02:32 -0600

    Added endpoint /{election_id}/participation-by-role/ in election controller

--- app/interfaces/election_controller.py
+++ app/interfaces/election_controller.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, HTTPException
 from app.application.query_bus import query_bus
-from app.application.queries import CandidateSupportQuery, ElectionSummaryQuery, ElectionTurnoutQuery, GetAllElectionsQuery, GetElectionDetailsQuery, GetElectionResultsQuery, TopCandidateQuery
+from app.application.queries import CandidateSupportQuery, ElectionSummaryQuery, ElectionTurnoutQuery, GetAllElectionsQuery, GetElectionDetailsQuery, GetElectionResultsQuery, ParticipationByRoleQuery, TopCandidateQuery
 from app.application.commands import CreateElectionCommand, EndElectionCommand
 from app.infrastructure.models import ElectionResponse
 from app.application.commands import CreateElectionCommand
@@ -106,6 +106,20 @@ def election_summary():
 @router.get("/{election_id}/top-candidate/")
 def top_candidate(election_id: int):
     query = TopCandidateQuery(election_id=election_id)
+    try:
+        result = query_bus.handle(query)
+        return result
+    except ValueError as e:
+        raise HTTPException(status_code=404, detail=str(e))
+    except Exception as e:
+        raise HTTPException(status_code=500, detail="An unexpected error occurred.")
+    
+@router.get("/{election_id}/participation-by-role/")
+def participation_by_role(
+    election_id: int
+):
+    query = ParticipationByRoleQuery(election_id=election_id)
+
     try:
         result = query_bus.handle(query)
         return result
