b768530ef6c5af59023afdd700dfec5cd251f23f
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-30 03:55:24 -0600

    Added PollingStationRepository in infrastucture layer

---
+++ app/infrastructure/polling_station_repo.py
@@ -0,0 +1,12 @@
+from sqlalchemy.orm import Session
+from app.infrastructure.models import PollingStation
+class PollingStationRepository:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_polling_station(self, name: str, location: str, election_id: int, capacity: int):
+        polling_station = PollingStation(name=name, location=location, election_id=election_id, capacity=capacity)
+        self.db.add(polling_station)
+        self.db.commit()
+        self.db.refresh(polling_station)
+        return polling_station
