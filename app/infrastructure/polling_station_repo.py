1d087cc1090820d03067d37427968bc54bf1a4f9
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-04-30 03:58:09 -0600

    Added update_polling_station in polling repo

--- app/infrastructure/polling_station_repo.py
+++ app/infrastructure/polling_station_repo.py
@@ -16,3 +16,18 @@ class PollingStationRepository:
     
     def get_polling_stations_by_election(self, election_id: int):
         return self.db.query(PollingStation).filter(PollingStation.election_id == election_id).all()
+    
+    def update_polling_station(self, station_id: int, name: str = None, location: str = None, capacity: int = None):
+        polling_station = self.get_polling_station_by_id(station_id)
+        if not polling_station:
+            return None
+
+        if name:
+            polling_station.name = name
+        if location:
+            polling_station.location = location
+        if capacity:
+            polling_station.capacity = capacity
+
+        self.db.commit()
+        return polling_station
