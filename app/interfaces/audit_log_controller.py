7208f4347cd73bf3f7393964d3c94b5876376237
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-02 21:17:41 -0600

    added endpoint get_audit_logs in controller

--- app/interfaces/audit_log_controller.py
+++ app/interfaces/audit_log_controller.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.application.commands import CreateAuditLogCommand
+from app.application.queries import GetAuditLogsQuery
 from app.application.query_bus import query_bus
 from app.infrastructure.database import get_db
 from fastapi import Form
@@ -18,3 +19,8 @@ def create_audit_log(query: CreateAuditLogCommand, db: Session = Depends(get_db)
         return command_bus.handle(query)
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/elections/{election_id}/audit-logs")
+def get_audit_logs(election_id: int, db: Session = Depends(get_db)):
+    query = GetAuditLogsQuery(election_id=election_id)
+    return query_bus.handle(query)
