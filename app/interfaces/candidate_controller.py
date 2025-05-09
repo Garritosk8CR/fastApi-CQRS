b994b13764def7606b510ea43ad3874922f2042f
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 14:54:50 -0600

    Cleaned imports not being used

--- app/interfaces/candidate_controller.py
+++ app/interfaces/candidate_controller.py
@@ -1,14 +1,12 @@
-from fastapi import APIRouter, HTTPException, Depends, Query, Request
-from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import APIRouter, HTTPException, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.application.commands import CreateCandidateCommand, DeleteCandidateCommand, UpdateCandidateCommand
 from app.application.queries import GetCandidatesQuery
 from app.application.query_bus import query_bus
 from app.infrastructure.database import get_db
-from fastapi import Form
 from app.application.handlers import command_bus
-from app.infrastructure.models import Observer
+
 
 router = APIRouter(prefix="/candidates", tags=["Candidate"])
 templates = Jinja2Templates(directory="app/templates")
