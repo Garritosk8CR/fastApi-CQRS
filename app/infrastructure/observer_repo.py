f0e6e83a64b80fc3bbd4552721ec1974e0cfff98
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 03:38:46 -0600

    Added create_observer in observer repo

--- app/infrastructure/observer_repo.py
+++ app/infrastructure/observer_repo.py
@@ -4,3 +4,10 @@ from app.infrastructure.models import Observer
 class ObserverRepository:
     def __init__(self, db: Session):
         self.db = db
+
+    def create_observer(self, name: str, email: str, election_id: int, organization: str = None):
+        observer = Observer(name=name, email=email, election_id=election_id, organization=organization)
+        self.db.add(observer)
+        self.db.commit()
+        self.db.refresh(observer)
+        return observer
