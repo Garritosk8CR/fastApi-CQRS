0a9828c1af4393b3b98fe7026010d5ac3d8fdd7d
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 12:24:47 -0600

    Added candidate query handler in query bus

--- app/application/handlers.py
+++ app/application/handlers.py
@@ -830,6 +830,7 @@ query_bus.register_handler(GetAuditLogsQuery, GetAuditLogsHandler())
 query_bus.register_handler(ExportElectionResultsQuery, ExportElectionResultsHandler())
 query_bus.register_handler(GetObserversQuery, GetObserversHandler())
 query_bus.register_handler(GetObserverByIdQuery, GetObserverByIdHandler())
+query_bus.register_handler(GetCandidatesQuery, GetCandidatesHandler())
 
 
 
