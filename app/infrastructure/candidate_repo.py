b3516f3511b9faf151f1f382b17fc26b28c6b81f
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 11:41:51 -0600

    Added create_candidate method in candidate repo

--- app/infrastructure/candidate_repo.py
+++ app/infrastructure/candidate_repo.py
@@ -1,5 +1,14 @@
 from sqlalchemy.orm import Session
 
+from app.infrastructure.models import Candidate
+
 class CandidateRepository:
     def __init__(self, db: Session):
         self.db = db
+
+    def create_candidate(self, name: str, party: str, bio: str, election_id: int):
+        candidate = Candidate(name=name, party=party, bio=bio, election_id=election_id)
+        self.db.add(candidate)
+        self.db.commit()
+        self.db.refresh(candidate)
+        return candidate
