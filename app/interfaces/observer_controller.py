52d5864f2695216af1b6547f398584a3814c7ca0
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 05:37:17 -0600

    Added create_observer endpoint

--- app/interfaces/observer_controller.py
+++ app/interfaces/observer_controller.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends, Query, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
+from app.application.commands import CreateObserverCommand
 from app.application.query_bus import query_bus
 from app.infrastructure.database import get_db
 from fastapi import Form
@@ -9,3 +10,10 @@ from app.application.handlers import command_bus
 
 router = APIRouter(prefix="/observer", tags=["Observer"])
 templates = Jinja2Templates(directory="app/templates")
+
+@router.post("/")
+def create_observer(query: CreateObserverCommand, db: Session = Depends(get_db)):
+    try:
+        return command_bus.handle(query)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
