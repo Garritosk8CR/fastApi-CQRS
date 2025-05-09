fbcb946fbc3818028f3444e49fa476ae6b7d98ef
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 14:48:09 -0600

    Added create_candidate endpoint in candidate controller

--- app/interfaces/candidate_controller.py
+++ app/interfaces/candidate_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends, Query, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
-from app.application.commands import CreateObserverCommand, DeleteObserverCommand, UpdateObserverCommand
+from app.application.commands import CreateCandidateCommand, CreateObserverCommand, DeleteObserverCommand, UpdateObserverCommand
 from app.application.queries import GetObserverByIdQuery, GetObserversQuery
 from app.application.query_bus import query_bus
 from app.infrastructure.database import get_db
@@ -12,3 +12,10 @@ from app.infrastructure.models import Observer
 
 router = APIRouter(prefix="/candidates", tags=["Candidate"])
 templates = Jinja2Templates(directory="app/templates")
+
+@router.post("/")
+def create_candidate(query: CreateCandidateCommand, db: Session = Depends(get_db)):
+    try:
+        return command_bus.handle(query)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
