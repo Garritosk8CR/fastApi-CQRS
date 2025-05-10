4e83302d28755ca3fa27cc699d8d191fa117f503
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-09 22:51:28 -0600

    Added cast_vote in vote repo

--- app/infrastructure/vote_repo.py
+++ app/infrastructure/vote_repo.py
@@ -1,5 +1,14 @@
 from sqlalchemy.orm import Session
 
+from app.infrastructure.models import Vote
+
 class VoteRepository:
     def __init__(self, db: Session):
         self.db = db
+
+    def cast_vote(self, voter_id: int, candidate_id: int, election_id: int):
+        vote = Vote(voter_id=voter_id, candidate_id=candidate_id, election_id=election_id)
+        self.db.add(vote)
+        self.db.commit()
+        self.db.refresh(vote)
+        return vote
