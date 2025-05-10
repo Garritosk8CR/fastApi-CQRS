064e48003fff180b2990e04c657c6c386493ff18
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-10 00:15:28 -0600

    Added get_votes_by_voter endpoint in vote controller

--- app/interfaces/vote_controller.py
+++ app/interfaces/vote_controller.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException, Depends
 from fastapi.templating import Jinja2Templates
 from sqlalchemy.orm import Session
 from app.application.commands import CastVoteCommand
-from app.application.queries import GetVotesByElectionQuery
+from app.application.queries import GetVotesByElectionQuery, GetVotesByVoterQuery
 from app.application.query_bus import query_bus
 from app.infrastructure.database import get_db
 from app.application.handlers import command_bus
@@ -20,4 +20,9 @@ def cast_vote(query: CastVoteCommand, db: Session = Depends(get_db)):
 @router.get("/elections/{election_id}/votes")
 def get_votes_by_election(election_id: int, db: Session = Depends(get_db)):
     query = GetVotesByElectionQuery(election_id=election_id)
+    return query_bus.handle(query)
+
+@router.get("/voters/{voter_id}/votes")
+def get_votes_by_voter(voter_id: int, db: Session = Depends(get_db)):
+    query = GetVotesByVoterQuery(voter_id=voter_id)
     return query_bus.handle(query)
