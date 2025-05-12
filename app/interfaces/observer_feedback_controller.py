784e430aee652c7f9c6f671770058abf38099f82
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-11 22:50:24 -0700

    Added submit_feedback in observer feedback controller

--- app/interfaces/observer_feedback_controller.py
+++ app/interfaces/observer_feedback_controller.py
@@ -6,5 +6,12 @@ from app.application.query_bus import query_bus
 from app.infrastructure.database import get_db
 from app.application.handlers import command_bus
 
-router = APIRouter(prefix="/feedback", tags=["Feedback"])
+router = APIRouter(prefix="/observer_feedback", tags=["Feedback"])
 templates = Jinja2Templates(directory="app/templates")
+
+@router.post("/")
+def submit_feedback(query: SubmitFeedbackCommand):
+    try:
+        return command_bus.handle(query)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
