f8052bb4d8b648e0c230670faaa4ddda6c27aeb0
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-24 21:55:31 -0700

    Added RealTimeElectionSummaryHandler to the query bus

--- app/application/handlers.py
+++ app/application/handlers.py
@@ -1033,6 +1033,7 @@ query_bus.register_handler(GetTurnoutConfidenceQuery, GetTurnoutConfidenceHandle
 query_bus.register_handler(GetDetailedHistoricalComparisonsQuery, GetDetailedHistoricalComparisonsHandler())
 query_bus.register_handler(GetDetailedHistoricalComparisonsWithExternalQuery, GetDetailedHistoricalComparisonsWithExternalHandler())
 query_bus.register_handler(DashboardAnalyticsQuery, DashboardAnalyticsHandler())
+query_bus.register_handler(RealTimeElectionSummaryQuery, RealTimeElectionSummaryHandler())
 
 
 
