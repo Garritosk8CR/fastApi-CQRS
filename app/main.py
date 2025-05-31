4b6fdc86cdfc25bb4e5b4514150f751abf9181b9
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-31 02:38:11 -0700

    Registered alert router in main

--- app/main.py
+++ app/main.py
@@ -15,6 +15,7 @@ from app.interfaces.observer_controller import router as observer_router
 from app.interfaces.candidate_controller import router as candidate_router
 from app.interfaces.vote_controller import router as votes_router
 from app.interfaces.observer_feedback_controller import router as observer_feedback_router
+from app.interfaces.alert_controller import router as alert_router
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
@@ -35,6 +36,7 @@ app.include_router(observer_router)
 app.include_router(candidate_router)
 app.include_router(votes_router)
 app.include_router(observer_feedback_router)
+app.include_router(alert_router)
 
 
 # Create tables in the database
