9ab3d04238b30747ec9c387f085a47a7f98a3d5a
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-05-31 21:27:00 -0700

    Added endpoint list_notifications

--- app/interfaces/notification_controller.py
+++ app/interfaces/notification_controller.py
@@ -11,8 +11,13 @@ from app.application.queries import GetNotificationsQuery
 from app.application.query_bus import query_bus
 from app.infrastructure.database import SessionLocal, get_db
 from app.application.handlers import command_bus
-from app.infrastructure.models import Notification
+from app.infrastructure.models import Notification, NotificationResponse
 
 
 router = APIRouter(prefix="/notifications", tags=["Notifications"])
 templates = Jinja2Templates(directory="app/templates")
+
+@router.get("/", response_model=List[NotificationResponse])
+def list_notifications(user_id: int = Query(..., description="User ID to fetch notifications for")):
+    query = GetNotificationsQuery(user_id=user_id)
+    return query_bus.handle(query)
