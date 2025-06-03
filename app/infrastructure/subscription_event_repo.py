299204aee630f2aefc605db3ec2cad0017dad8c1
Emilio Garro Rangel <emonteror@ucenfotec.ac.cr>
2025-06-03 01:48:26 -0700

    Added Subscription event repo method get_subscription_analytics

--- app/infrastructure/subscription_event_repo.py
+++ app/infrastructure/subscription_event_repo.py
@@ -1,5 +1,5 @@
 import math
-from sqlalchemy import func
+from sqlalchemy import case, func
 from sqlalchemy.orm import Session
 from app.infrastructure.models import SubscriptionEvent
 
@@ -18,3 +18,21 @@ class SubscriptionEventRepository:
         self.db.commit()
         self.db.refresh(event)
         return event
+    
+    def get_subscription_analytics(self, user_id: int) -> list:
+        query = self.db.query(
+            SubscriptionEvent.alert_type,
+            func.count(SubscriptionEvent.id).label("total_changes"),
+            func.sum(case([(SubscriptionEvent.new_value == True, 1)], else_=0)).label("enabled_count"),
+            func.sum(case([(SubscriptionEvent.new_value == False, 1)], else_=0)).label("disabled_count")
+        ).filter(SubscriptionEvent.user_id == user_id).group_by(SubscriptionEvent.alert_type)
+        results = query.all()
+        return [
+            {
+                "alert_type": row[0],
+                "total_changes": row[1],
+                "enabled_count": row[2],
+                "disabled_count": row[3]
+            }
+            for row in results
+        ]
